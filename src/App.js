dcb21db41d08750e3f1b85a4bf3c7a8675716e80
Ahmad Internaldo <ahmadinternaldo@Ahmads-MacBook-Pro.local>
2021-07-27 17:53:33 +0700

    Making template

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="items">
+      <div className="testing">
+        <h1>Kulina</h1>
+      </div>
+      <h2 style={{textAlign:'center'}}>Product Card</h2>
+
+      <div className="card">
+        <img src="https://static.wixstatic.com/media/57689e_e899386abb0147fb95aae4fb0b7d6015~mv2.jpg/v1/crop/x_0,y_14,w_1200,h_933/fill/w_600,h_466,al_c,q_90/lauk.webp" alt="food" style={{width:'100%'}} />
+        <h2>3 <span> </span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star"></span>
+        <span className="fa fa-star"></span>
+        </h2>
+        <h1>Nasi Campur</h1>
+        <p>By Kulina Uptown Lunch.</p>
+        <div className="leftRight">
+          <p className="price">Rp50.000,00</p>
+          <button>Add +</button>
+        </div>
+      </div>
+      <h2 style={{textAlign:'center'}}>Product Card</h2>
+
+      <div className="card">
+        <img src="https://static.wixstatic.com/media/57689e_e899386abb0147fb95aae4fb0b7d6015~mv2.jpg/v1/crop/x_0,y_14,w_1200,h_933/fill/w_600,h_466,al_c,q_90/lauk.webp" alt="food" style={{width:'100%'}} />
+        <h2>3 <span> </span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star"></span>
+        <span className="fa fa-star"></span>
+        </h2>
+        <h1>Nasi Campur</h1>
+        <p>By Kulina Uptown Lunch.</p>
+        <div className="leftRight">
+          <p className="price">Rp50.000,00</p>
+          <button>Add +</button>
+        </div>
+      </div>
+      <h2 style={{textAlign:'center'}}>Product Card</h2>
+
+      <div className="card">
+        <img src="https://static.wixstatic.com/media/57689e_e899386abb0147fb95aae4fb0b7d6015~mv2.jpg/v1/crop/x_0,y_14,w_1200,h_933/fill/w_600,h_466,al_c,q_90/lauk.webp" alt="food" style={{width:'100%'}} />
+        <h2>3 <span> </span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star"></span>
+        <span className="fa fa-star"></span>
+        </h2>
+        <h1>Nasi Campur</h1>
+        <p>By Kulina Uptown Lunch.</p>
+        <div className="leftRight">
+          <p className="price">Rp50.000,00</p>
+          <button>Add +</button>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
